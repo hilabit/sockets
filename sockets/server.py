1b51f1b913dbd7dbbecb4a0f688bfa0b25fe7cc9
hilabit <hilabit@gmail.com>
2023-08-29 21:54:27 +0200

    parsing message from server

--- sockets/server.py
+++ sockets/server.py
@@ -1,4 +1,5 @@
 from twisted.internet import protocol, reactor, endpoints
+import generated.log_message_pb2 as log_message
 
 HOST = "127.0.0.1"
 PORT = 15000
@@ -6,7 +7,9 @@ PORT = 15000
 
 class Echo(protocol.Protocol):
     def dataReceived(self, data):
-        print(data)
+        log_msg = log_message.LogMessage()
+        log_msg.ParseFromString(data)
+        print(log_msg)
 
 
 class EchoFactory(protocol.Factory):
